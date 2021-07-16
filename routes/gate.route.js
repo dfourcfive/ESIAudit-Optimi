d5f3e9141922cd422660911ad4d0220ff5b9c82f
dfourcfive <i.benadidou@esi-sba.dz>
2021-07-16 19:57:41 +0100

    remove middleware

--- routes/gate.route.js
+++ routes/gate.route.js
@@ -1,7 +1,6 @@
-const { authJwt } = require("../middleware");
 const controller = require("../controllers/activite.controller");
 
 
 
 
-  app.get("/api/data/activites/:id",[authJwt.verifyToken],controller.getOne);
+  app.get("/api/data/activites/:id",controller.getOne);
